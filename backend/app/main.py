1eeea2c84cfdbe797dcc32c4ee8402b0ac4211dc
Aishwarya Bhargava <aishu51198@gmail.com>
2025-12-26 15:38:58 -0500

    Updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,11 +25,12 @@ app = FastAPI(
     version=APP_VERSION,
 )
 
-FRONTEND_URL = os.getenv("FRONTEND_URL", "BACKEND_URL", "http://localhost:5173", "http://127.0.0.1:5173")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
